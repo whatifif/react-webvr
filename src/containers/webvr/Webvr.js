f68ac707ed7b854014bd2c023d7f2b1ef6b07cdc
whatifif <whatifif@yahoo.com>
2016-02-14 16:55:53 +1100

    WebVRPage is working.

--- src/containers/webvr/Webvr.js
+++ src/containers/webvr/Webvr.js
@@ -1,18 +1,30 @@
 require('aframe-core');
 import {Animation, Entity, Scene} from 'aframe-react';
-import React from 'react';
 
-const Webvr = (props) => {
-  console.log(props);
-  if(false){
-     return (
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+// import User from '../../components/user/User';
+
+// import { invalidateWebVRPage, selectWebVRPage, fetchTopUsersIfNeeded } from '../../actions/users';
+
+class WebVRPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { vrMode } = this.props;
+    // console.log(this.props);
+    if(vrMode){
+    return (
         <Scene>
           <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
             <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
           </Entity>
         </Scene>
-    ); 
-  }else{
+    );
+    }else{
     return (
       <div className="container">
         <h1 className="display-3">WebVR
@@ -39,9 +51,71 @@ const Webvr = (props) => {
           and productive.
         </p>
       </div>
-    );
-  }
+    );      
+    }
 
+  }
+}
+
+WebVRPage.propTypes = {
+  vrMode: PropTypes.boolean
 };
 
-export default Webvr;
+function mapStateToProps(state) {
+  const { toggleWebVR } = state;
+
+  return {
+    vrMode: toggleWebVR.vrMode
+  };
+}
+
+export default connect(mapStateToProps)(WebVRPage);
+
+//------------------------------------------------------------------
+// require('aframe-core');
+// import {Animation, Entity, Scene} from 'aframe-react';
+// import React from 'react';
+
+// const Webvr = (props) => {
+//   console.log(props);
+//   if(false){
+//      return (
+//         <Scene>
+//           <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
+//             <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+//           </Entity>
+//         </Scene>
+//     ); 
+//   }else{
+//     return (
+//       <div className="container">
+//         <h1 className="display-3">WebVR
+//         </h1> 
+//         <p>
+//           Thanks for checking this out. This project was created by
+//           <a href="https://twitter.com/yunjun_mu" target="_blank"> @yunjun_mu</a>,
+//           as a playground to learn and experiment some of the exciting technologies such as
+//           <a href="https://github.com/facebook/react" target="_blank"> React</a> and
+//           <a href="https://github.com/rackt/redux" target="_blank"> Redux</a>.
+//         </p>
+
+//         <p>
+//           I believe these two libraries can serve as cornerstones to build a modern web application on, each
+//           addressing an important aspect of web development: <a href="https://github.com/facebook/react" target="_blank">React </a>
+//           as the V (view), and <a href="https://github.com/rackt/redux" target="_blank"> Redux </a> as the
+//           predictable state container.
+//         </p>
+
+//         <p>
+//           Along with other great libraries, such as <a href="https://github.com/rackt/react-router" target="_blank"> React-Router</a> for routing,
+//           <a href="http://babeljs.io" target="_blank"> Babel </a> for next-gen Javascript, and
+//           <a href="http://webpack.github.io" target="_blank"> Webpack</a> for bundling and devtools,  web development has never been more fun
+//           and productive.
+//         </p>
+//       </div>
+//     );
+//   }
+
+// };
+
+// export default Webvr;
--- src/reducers/webvr.js
+++ src/reducers/webvr.js
@@ -1,7 +1,8 @@
 'use strict';
 import { TOGGLE_WEBVR } from '../actions/webvr';
+const INITIAL_STATE = {vrMode:false};
 
-export function toggleWebVR(state = {vrMode:false}, action) {
+export function toggleWebVR(state = INITIAL_STATE, action) {
   switch (action.type) {
     case TOGGLE_WEBVR:
       return Object.assign({}, state, {vrMode: !state.vrMode});
