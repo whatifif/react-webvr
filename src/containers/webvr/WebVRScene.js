c22748687d749cfdd6c2be0e5b8d3af1661b4899
whatifif <whatifif@yahoo.com>
2016-02-15 11:54:40 +1100

    WebVRScene

--- src/containers/webvr/WebVRScene.js
+++ src/containers/webvr/WebVRScene.js
@@ -4,16 +4,16 @@ import React, { Component, PropTypes } from 'react';
 
 export default class WebVRScene extends Component {
 
-	render(){
-      return(
-              <div id='webvr'>
-                <Scene ref="scene">
-                  <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
-                    <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
-                  </Entity>
-                </Scene>
-              </div>
-              );
-	}
+  	render(){
+        return(
+                <div id='webvr'>
+                  <Scene ref="scene">
+                    <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
+                      <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+                    </Entity>
+                  </Scene>
+                </div>
+        );
+    }
 
   }
