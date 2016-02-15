08fdea972bab7136138dfea1a7838e0062d3a3c3
whatifif <whatifif@yahoo.com>
2016-02-15 11:40:48 +1100

    WebVRScene is separated from the WebVR page in order to achieve the lazy loading.

---
+++ src/containers/webvr/WebVRScene.js
@@ -0,0 +1,19 @@
+require('aframe-core');
+import {Animation, Entity, Scene} from 'aframe-react';
+import React, { Component, PropTypes } from 'react';
+
+export default class WebVRScene extends Component {
+
+	render(){
+      return(
+              <div id='webvr'>
+                <Scene ref="scene">
+                  <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
+                    <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+                  </Entity>
+                </Scene>
+              </div>
+              );
+	}
+
+  }
